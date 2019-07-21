dd8834010f355ac87177993d97b910d98bcb9ac5
bradev <rafaelperozin@outlook.com>
2019-07-21 16:14:10 +0100

    creating express API

---
+++ AngularApp/src/app/player.model.ts
@@ -0,0 +1,11 @@
+export class Player {
+    guid: any;
+    age: number;
+    yellowCards: number;
+    redCards: number;
+    goals: number;
+    name: string;
+    team: {
+        name: string;
+    }
+}
